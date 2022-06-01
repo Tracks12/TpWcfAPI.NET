0849954205028412f5b2cfd566fe47bb217a240f
Tracks12 <cardinal.florian.erwan@gmail.com>
2022-06-01 13:56:36 +0200

    Ajoutez des fichiers projet.

---
+++ TP_WCF/Models/TodoItemDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TP_WCF.Models
+{
+    public class TodoItemDTO
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool Done { get; set; }
+    }
+}
